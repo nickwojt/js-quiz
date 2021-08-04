487b6e97c1383a0ee558a4c1ef34ec197efbfd8a
Nicholas Wojtalewicz <nicholaswojt@outlook.com>
2021-08-04 17:11:51 -0600

    Made more changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,9 @@ var timer = 60;
 $('#timer').text(timer)
 var highscore = $('#highscores');
 var finalScore = localStorage.getItem('score');
-
+var scorePageEl = $('#score-page');
+var viewHighscores = $('#highscore');
+var index = 0;
 
 var questions = [
     {
@@ -38,7 +40,7 @@ var questions = [
         answer: 'alert("Hello World")'
     }
 ]
-
+viewHighscores.on('click', scorePage);
 startButton.on("click", startQuiz);
 questionEl.removeClass('hide');
 
@@ -47,128 +49,183 @@ function startQuiz() {
     questionContainerEl.removeClass('hide');
     questionButtonsEl.removeClass('hide');
     outputEl.removeClass('hide');
-    firstQuestion();
+    getQuestions();
 }
 
 
 
-function firstQuestion() {
-    
-    questionEl.text(questions[0].question);
-    choice[0].text(questions[0].choices[0]);
-    choice[1].text(questions[0].choices[1]);
-    choice[2].text(questions[0].choices[2]);
-    choice[3].text(questions[0].choices[3]);
+// function firstQuestion() {
+//     console.log(score)
+//     questionEl.text(questions[0].question);
+//     choice[0].text(questions[0].choices[0]);
+//     choice[1].text(questions[0].choices[1]);
+//     choice[2].text(questions[0].choices[2]);
+//     choice[3].text(questions[0].choices[3]);
 
 
-    answerButtonsEl.on('click', function(event){
-        event.preventDefault();
-        var selection = $(event.target).text();
-        if (selection === questions[0].answer) {
-            score++;
-            $('#score').text(score);
-            outputEl.text("Correct");
-            secondQuestion();
-        } else {
-            timer -= 15;
-            outputEl.text("Wrong");
-            secondQuestion();
-        }
-    })
-}
-
-
-
-function secondQuestion() {
-    
-    questionEl.text(questions[1].question);
-    choice[0].text(questions[1].choices[0]);
-    choice[1].text(questions[1].choices[1]);
-    choice[2].text(questions[1].choices[2]);
-    choice[3].text(questions[1].choices[3]);
-
-
-    answerButtonsEl.on('click', function(event){
-        event.preventDefault();
-        var selection = $(event.target).text();
-        if (selection === questions[1].answer) {
-            score++;
-            $('#score').text(score);
-            outputEl.text("Correct");
-            thirdQuestion();
-        } else {
-            timer -= 15;
-            outputEl.text("Wrong");
-            thirdQuestion();
-        }
-    })
-}
+//     answerButtonsEl.on('click', function(event){
+//         event.preventDefault();
+//         var selection = $(event.target).text();
+//         if (selection === questions[0].answer) {
+//             score++;
+//             $('#score').text(score);
+//             outputEl.text("Correct");
+//             return secondQuestion();
+//         } else {
+//             timer -= 15;
+//             outputEl.text("Wrong");
+//             return secondQuestion();
+//         }
+//     })
+// }
 
 
 
-function thirdQuestion() {
-    
-    questionEl.text(questions[2].question);
-    choice[0].text(questions[2].choices[0]);
-    choice[1].text(questions[2].choices[1]);
-    choice[2].text(questions[2].choices[2]);
-    choice[3].text(questions[2].choices[3]);
+// function secondQuestion() {
+//     console.log(score)
+//     questionEl.text(questions[1].question);
+//     choice[0].text(questions[1].choices[0]);
+//     choice[1].text(questions[1].choices[1]);
+//     choice[2].text(questions[1].choices[2]);
+//     choice[3].text(questions[1].choices[3]);
 
 
-    answerButtonsEl.on('click', function(event){
-        event.preventDefault();
-        var selection = $(event.target).text();
-        if (selection === questions[2].answer) {
-            score++;
-            $('#score').text(score);
-            outputEl.text("Correct");
-            fourthQuestion();
-        } else {
-            timer -= 15;
-            outputEl.text("Wrong");
-            fourthQuestion();
-        }
-    })
-}
+//     answerButtonsEl.on('click', function(event){
+//         event.preventDefault();
+//         var selection = $(event.target).text();
+//         if (selection === questions[1].answer) {
+//             score++;
+//             $('#score').text(score);
+//             outputEl.text("Correct");
+//             return thirdQuestion();
+//         } else {
+//             timer -= 15;
+//             outputEl.text("Wrong");
+//             return thirdQuestion();
+//         } 
+//     })
+// }
 
 
 
-function fourthQuestion() {
-    
-    questionEl.text(questions[3].question);
-    choice[0].text(questions[3].choices[0]);
-    choice[1].text(questions[3].choices[1]);
-    choice[2].text(questions[3].choices[2]);
-    choice[3].text(questions[3].choices[3]);
+// function thirdQuestion() {
+//     console.log(score)
+//     questionEl.text(questions[2].question);
+//     choice[0].text(questions[2].choices[0]);
+//     choice[1].text(questions[2].choices[1]);
+//     choice[2].text(questions[2].choices[2]);
+//     choice[3].text(questions[2].choices[3]);
 
 
-    answerButtonsEl.on('click', function(event){
-        event.preventDefault();
-        var selection = $(event.target).text();
-        if (selection === questions[3].answer) {
-            score++;
-            $('#score').text(score);
-            outputEl.text("Correct");
-            highscores();
-        } else {
-            timer -= 15;
-            outputEl.text("Wrong");
-            highscores();
-        }
-    })
-}
+//     answerButtonsEl.on('click', function(event){
+//         event.preventDefault();
+//         var selection = $(event.target).text();
+//         if (selection === questions[2].answer) {
+//             score++;
+//             $('#score').text(score);
+//             outputEl.text("Correct");
+//             return fourthQuestion();
+//         } else {
+//             timer -= 15;
+//             outputEl.text("Wrong");
+//             return fourthQuestion();
+//         }
+//     })
+// }
+
+
+
+// function fourthQuestion() {
+//     console.log(score)
+//     questionEl.text(questions[3].question);
+//     choice[0].text(questions[3].choices[0]);
+//     choice[1].text(questions[3].choices[1]);
+//     choice[2].text(questions[3].choices[2]);
+//     choice[3].text(questions[3].choices[3]);
+
+
+//     answerButtonsEl.on('click', function(event){
+//         event.preventDefault();
+//         var selection = $(event.target).text();
+//         console.log($(event.target).text());
+//         if (selection === questions[3].answer) {
+//             score++;
+//             $('#score').text(score);
+//             outputEl.text("Correct");
+//             return highscores();
+//         } else {
+//             timer -= 15;
+//             outputEl.text("Wrong");
+//             console.log(score)
+//             return highscores();
+//         }
+//     })
+// }
 
 
 function highscores() {
     questionEl.addClass('hide');
     questionButtonsEl.addClass('hide');
     outputEl.addClass('hide');
-    highscore.removeClass('show-end');
-    var initials = $('#input').val()
-    console.log(initials)
-    localStorage.setItem('initials', initials)
-    highscore.append('<div>' + initials + ' = ' + score)
-
+    highscore.removeClass('hide');
+    highscore.on('submit', function(event){
+        event.preventDefault();
+        var initials = $('#initials').val()
+        localStorage.setItem('initials', initials);
+        scorePageEl.append('<div>' + initials + ' = ' + score + '</div>');
+        scorePage();
+    })
 }
 
-//got to figure out input and button values and diplay them, local storage, timer, and view highscores screen
+
+
+function scorePage() {
+    startButton.addClass('hide');
+    questionEl.addClass('hide');
+    questionButtonsEl.addClass('hide');
+    outputEl.addClass('hide');
+    highscore.addClass('hide');
+    scorePageEl.removeClass('hide');
+    $('#back').on('click', function(){
+        window.location.replace('index.html');
+    })
+    
+}
+
+
+function getQuestions() {
+    var currentQuestion = questions[index];
+    questionEl.text(currentQuestion.question);
+    choice[0].text(currentQuestion.choices[0]);
+    choice[1].text(currentQuestion.choices[1]);
+    choice[2].text(currentQuestion.choices[2]);
+    choice[3].text(currentQuestion.choices[3]);
+    answerButtonsEl.on('click', selectAnswer);
+}
+
+
+function selectAnswer(event) {
+        event.preventDefault();
+        var selection = $(event.target).text();
+        console.log($(event.target).text());
+        // console.log(this.value)
+        if (selection === questions[index].answer) {
+            score++;
+            $('#score').text(score);
+            outputEl.text("Correct");
+            
+        } else {
+            timer -= 15;
+            outputEl.text("Wrong");
+            // console.log(score)
+            
+        }
+    
+    index++;
+    console.log(index)
+    if (index === questions.length) {
+        highscores();
+    } else {
+        getQuestions();
+    }
+}
